b2b39c22f5612d302d24b9dd5c5b3b474b0e1fe3
DESKTOP-GC9Q8CO <dariys.fanat@mail.ru>
2022-02-18 19:41:35 +0300

    add tests

---
+++ View/UnitTestProject1/CarTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Набор тестов для класса Hour
+    /// </summary>
+    [TestFixture]
+    public class CarTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
